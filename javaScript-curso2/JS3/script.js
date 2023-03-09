f42c55f6afec0a092453307261bc124c8841f35f
luizh3nrique <luizh3nrique00@gmail.com>
2023-03-09 12:06:45 -0300

    adcionei outro exercisio

--- javaScript-curso2/JS3/script.js
+++ javaScript-curso2/JS3/script.js
@@ -1,9 +1,19 @@
-var hora = 00
+var hora = new Date().getHours();
 
 if (hora < 12) {
     alert('bom dia')
 } else if (hora < 18) {
-    alert('boa tarde')
+    aalert('boa tarde')
 } else {
     alert('boa noite')
+}
+
+function verificar() {
+    let nome = document.getElementById('nome').value;
+
+    if (nome == "" || nome == null) {
+        let p = document.getElementById('texto').innerHTML = "o campo esta vazio";
+    } else {
+        let p = document.getElementById('texto').innerHTML = "tudo certo foi enviado com sucesso";
+    }
 }
